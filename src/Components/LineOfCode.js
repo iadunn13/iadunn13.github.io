7bc54b61d76d164ecc60e99a16b60177f9e70d09
Isaac Dunn <iadunn13@gmail.com>
2018-02-02 03:47:43 -0500

    Added LineOfCode.js

---
+++ src/Components/LineOfCode.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import './sass/LineOfCode.css';
+
+
+export default class LineOfCode extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.render = () => {
+			return (
+				<Row>
+					<Col xs={1}>
+						<p className="line-number">{this.props.lineNumber}</p>
+					</Col>
+					<Col xs={11}>
+						<p className="code">{this.props.code}</p>
+					</Col>
+				</Row>
+			)
+		}
+	}
+};
